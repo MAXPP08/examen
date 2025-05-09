10ea0605650d33cd0c8150dc2289a6ddaedd4312
MAXIMILIANO PACHECO PEREZ <101135113+MAXPP08@users.noreply.github.com>
2025-05-09 17:55:42 -0600

    Update test_fibonacci.py

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,15 +1,15 @@
 import timeit
-2 from fibonacci import fibonacci
-3
-4 def test_fibonacci () :
-5 assert fibonacci (0) == 0
-6 assert fibonacci (1) == 1
-7 assert fibonacci (5) == 5
-8 print (" Pruebas pasadas !")
-9
-10 if __name__ == " __main__ ":
-11 test_fibonacci ()
-12 time = timeit . timeit (" fibonacci (20) ",
-13 setup =" from fibonacci import fibonacci ",
-14 number =100)
-15 print (f" Tiempo : { time :.4 f} segundos ")
+ from fibonacci import fibonacci
+
+ def test_fibonacci () :
+ assert fibonacci (0) == 0
+ assert fibonacci (1) == 1
+ assert fibonacci (5) == 5
+ print (" Pruebas pasadas !")
+
+ if __name__ == " __main__ ":
+ test_fibonacci ()
+ time = timeit . timeit (" fibonacci (20) ",
+ setup =" from fibonacci import fibonacci ",
+ number =100)
+ print (f" Tiempo : { time :.4 f} segundos ")
